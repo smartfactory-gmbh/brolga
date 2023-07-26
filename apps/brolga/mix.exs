45516c567e2f22aae4cdd2f4a9cd111d5638f326
Justin Köstinger <justin+gitlab@smartfactory.ch>
2023-07-26 13:28:37 +0200

    feat: add timex deps

--- apps/brolga/mix.exs
+++ apps/brolga/mix.exs
@@ -41,7 +41,8 @@ defmodule Brolga.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:jason, "~> 1.2"},
       {:swoosh, "~> 1.3"},
-      {:finch, "~> 0.13"}
+      {:finch, "~> 0.13"},
+      {:timex, "~> 3.0"}
     ]
   end
 
