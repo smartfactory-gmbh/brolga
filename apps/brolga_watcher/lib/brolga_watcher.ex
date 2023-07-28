ca8813b72fb80f9f0224783d90b3d4448d15825c
Justin Köstinger <justin+gitlab@smartfactory.ch>
2023-07-28 13:13:21 +0200

    refactor: code cleanup

--- apps/brolga_watcher/lib/brolga_watcher.ex
+++ apps/brolga_watcher/lib/brolga_watcher.ex
@@ -2,17 +2,4 @@ defmodule BrolgaWatcher do
   @moduledoc """
   Documentation for `BrolgaWatcher`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> BrolgaWatcher.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
